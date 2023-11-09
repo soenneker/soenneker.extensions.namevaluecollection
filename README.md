419dcbb866c2233b0b036e9d802fbf474beb244f
Jake Soenneker <jake@soenneker.com>
2023-11-09 11:32:43 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.NameValueCollection.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.NameValueCollection/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.namevaluecollection/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.namevaluecollection/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.NameValueCollection.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.NameValueCollection/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Extensions.NameValueCollection
+### A collection of helpful NameValueCollection extension methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.NameValueCollection
+```
---
+++ src/NameValueCollectionExtension.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Soenneker.Extensions.NameValueCollection;
+
+/// <summary>
+/// A collection of helpful NameValueCollection extension methods
+/// </summary>
+public static class NameValueCollectionExtension
+{
+    /// <summary>
+    /// NameValueCollection can contain multiple equal keys, but dictionaries cannot. <para/>
+    /// So instead of returning a comma separate list for a value, keys that already exist in the Dictionary will not be added.<para/>
+    /// Will not add keys where the value is null either.
+    /// </summary>
+    public static Dictionary<string, string> ToDictionary(this System.Collections.Specialized.NameValueCollection nameValueCollection)
+    {
+        var result = new Dictionary<string, string>();
+
+        // check for count first because AllKeys is more expensive 
+        if (nameValueCollection.Count == 0)
+            return result;
+
+        foreach (string? key in nameValueCollection.AllKeys)
+        {
+            if (key == null)
+                continue;
+
+            if (result.ContainsKey(key))
+                continue;
+
+            string? value = nameValueCollection[key];
+
+            if (value == null)
+                continue;
+
+            result[key] = value;
+        }
+
+        return result;
+    }
+}
