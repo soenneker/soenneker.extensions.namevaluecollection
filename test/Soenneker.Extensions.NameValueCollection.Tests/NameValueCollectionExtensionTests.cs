bdd42418622c664fdb0e2b8a72ea0361aee39d3f
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:22 +0000

    Tests

---
+++ test/Soenneker.Extensions.NameValueCollection.Tests/NameValueCollectionExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.NameValueCollection.Tests;
+
+public class NameValueCollectionExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
